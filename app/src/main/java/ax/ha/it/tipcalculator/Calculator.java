100926b77b338ed10e5005904bbf8c9fb8376888
SpookyNoodlez <55265731+SpookyNoodlez@users.noreply.github.com>
2022-04-06 16:22:22 +0300

    Crashes on button press

---
+++ app/src/main/java/ax/ha/it/tipcalculator/Calculator.java
@@ -0,0 +1,7 @@
+package ax.ha.it.tipcalculator;
+
+public class Calculator {
+    public double calculateTip(double bill, double percent){
+        return bill*(percent/100);
+    }
+}
--- app/src/main/java/ax/ha/it/tipcalculator/MainActivity.java
+++ app/src/main/java/ax/ha/it/tipcalculator/MainActivity.java
@@ -3,12 +3,33 @@ package ax.ha.it.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+
+import ax.ha.it.tipcalculator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Calculator calculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonCalculate.setOnClickListener(view -> {
+            String billText = binding.tipLessBill.getText().toString();
+            double bill = Double.parseDouble(billText);
+            String percentText = binding.tipPercentage.getText().toString();
+            double percent = Double.parseDouble(percentText);
+
+            double tip = calculator.calculateTip(bill, percent);
+            double total = bill + tip;
+
+            binding.tipDisplay.setText(Double.toString(tip));
+            binding.resultDisplay.setText(Double.toString(total));
+        });
+
     }
 }
